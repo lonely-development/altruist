c000df10efbc37688ad54afe9b438ee5f48869b9
goncalo <goncalo@anewspring.nl>
2020-09-05 14:16:52 +0100

    * Add greater than 0 test cases.

--- src/test/groovy/com/altruist/trade/TradeControllerTest.groovy
+++ src/test/groovy/com/altruist/trade/TradeControllerTest.groovy
@@ -72,6 +72,34 @@ class TradeControllerTest extends Specification {
         results.andExpect(content().json("""{"id":"$expectedId"}"""))
     }
 
+    def "Should not accept trade field #field should be greater than zero"() {
+
+        given: "an account missing fields"
+        TradeDto request = new TradeDto(
+                id: UUID.randomUUID(),
+                accountId: UUID.randomUUID(),
+                quantity: 4,
+                price: 1,
+                symbol: TradeEntity.Symbol.MSFT,
+                side: TradeEntity.Side.BUY,
+                status: TradeEntity.Status.COMPLETED
+        )
+        request[field] = 0
+
+        when:
+        ResultActions results = mvc.perform(post("/trades")
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        )
+
+        then:
+        results.andExpect(status().isBadRequest())
+
+        where:
+        field << ["quantity", "price"]
+    }
+
     def "When cancel operation fails BAD_REQUEST should be returned"() {
         given: "a trade cancel request"
         UUID expectedId = UUID.randomUUID()
